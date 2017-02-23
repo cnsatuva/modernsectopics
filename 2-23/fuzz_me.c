d9e78b39f4f40c4fa5df0c1fbe1e1a0fca44b36a
Collin Berman <collinberman1@gmail.com>
2017-02-23 16:20:48 -0500

    add 2/23 slides and homework program

---
+++ 2-23/fuzz_me.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    char data[10] = {0};
+    read(0, data, 9);
+    data[9] = 0;
+
+    if (data[0] == 0x55) {
+        printf("First - %d\n", data[0]/data[9]);
+    } else if (data[0] == 0x55 && data[1] == 0xc5) {
+        printf("Second - %d\n", data[1]/data[9]);
+    } else {
+        printf("Nope! - %s\n", data);
+    }
+
+    return 0;
+}
