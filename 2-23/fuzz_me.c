3b0f3bd9a71d2196a5a7b425f2f2ec9f97b99a64
Collin Berman <collinberman1@gmail.com>
2017-02-23 17:51:22 -0500

    make second vuln possible to reach

--- 2-23/fuzz_me.c
+++ 2-23/fuzz_me.c
@@ -8,7 +8,7 @@ int main() {
 
     if (data[0] == 0x55) {
         printf("First - %d\n", data[0]/data[9]);
-    } else if (data[0] == 0x55 && data[1] == 0xc5) {
+    } else if (data[0] == 0x5c && data[1] == 0xc5) {
         printf("Second - %d\n", data[1]/data[9]);
     } else {
         printf("Nope! - %s\n", data);
